af1fd6954f89572e9e264766cbec468ce706270e
altescy <altescy@fastmail.com>
2020-05-30 20:34:56 +0900

    add abstract class for twitter crawlers

--- mincrawler/crawlers/twitter/tweet_search.py
+++ mincrawler/crawlers/twitter/tweet_search.py
@@ -1,21 +1,17 @@
-import copy
 import logging
 import typing as tp
-from urllib.parse import urlencode
 
 import colt
 import httpx
-from authlib.integrations.httpx_client import OAuth1Auth
-
-from mincrawler.crawlers.crawler import Crawler
+from mincrawler.crawlers.twitter.twitter_crawler import TwitterCrawler
 from mincrawler.item import Item
 
 logger = logging.getLogger(__name__)
 
 
 @colt.register("twitter_tweet_search")
-class TwittwerTweetSearchCrawler(Crawler):
-    TWEET_SEARCH_URL = "https://api.twitter.com/{version}/search/tweets.json"
+class TwittwerTweetSearchCrawler(TwitterCrawler):
+    RESOURCE_URL = "https://api.twitter.com/{version}/search/tweets.json"
 
     def __init__(self,
                  client_id: str,
@@ -25,10 +21,9 @@ class TwittwerTweetSearchCrawler(Crawler):
                  max_requests: int = None,
                  version: str = "1.1",
                  **params) -> None:
-        self._auth = OAuth1Auth(client_id, client_secret, token, token_secret)
+        super().__init__(client_id, client_secret, token, token_secret,
+                         version, **params)
         self._max_requests = max_requests
-        self._version = version
-        self._params = params
 
     def _run(self) -> tp.Iterator[Item]:
         for tweets in self._crawl():
@@ -37,15 +32,6 @@ class TwittwerTweetSearchCrawler(Crawler):
                 content = tweet
                 yield Item(item_id, content)
 
-    def _get_url(self, params: tp.Dict[str, tp.Any] = None) -> str:
-        if params is None:
-            params = self._params
-
-        base_url = self.TWEET_SEARCH_URL.format(version=self._version)
-        query_string = urlencode(params)
-
-        return f"{base_url}?{query_string}"
-
     def _get_max_requests(self) -> tp.Union[int, float]:
         if self._max_requests is None or self._max_requests < 0:
             return float("inf")
@@ -53,10 +39,9 @@ class TwittwerTweetSearchCrawler(Crawler):
 
     def _crawl(self) -> tp.Iterator[tp.List[tp.Dict[str, tp.Any]]]:
         max_requests = self._get_max_requests()
-        params = copy.deepcopy(self._params)
 
         num_requests = 0
-        url = self._get_url(params)
+        url = self.get_url()
         while num_requests < max_requests:
             logger.debug("request url: %s", url)
 
@@ -73,6 +58,6 @@ class TwittwerTweetSearchCrawler(Crawler):
             if "next_results" not in meta:
                 break
 
-            url = self.TWEET_SEARCH_URL.format(version=self._version) \
+            url = self.RESOURCE_URL.format(version=self._version) \
                     + meta["next_results"]
             num_requests += 1
