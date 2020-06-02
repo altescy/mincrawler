ad9bfce25aa6c61bf81b2f3ae571505dc8f79570
altescy <altescy@fastmail.com>
2020-06-02 22:14:36 +0900

    use  instead of

--- mincrawler/storages/tinydb_storage.py
+++ mincrawler/storages/tinydb_storage.py
@@ -1,4 +1,4 @@
-from tinydb import TinyDB, Query
+from tinydb import TinyDB, where
 
 from mincrawler.item import Item
 from mincrawler.storages.storage import Storage
@@ -20,20 +20,16 @@ class TinyDBStorage(Storage):
 
     def upsert(self, collection: str, item: Item) -> None:
         table = self._db.table(collection)
-        query = Query()
-        table.upsert(item.to_dict(), query[Item.ITEM_ID_KEY] == item.id)
+        table.upsert(item.to_dict(), where(Item.ITEM_ID_KEY) == item.id)
 
     def exists(self, collection: str, item: Item) -> bool:
         table = self._db.table(collection)
-        query = Query()
-        items = table.search(query[Item.ITEM_ID_KEY] == item.id)
+        items = table.search(where(Item.ITEM_ID_KEY) == item.id)
         return len(items) > 0
 
     def get(self, collection: str, item_id: str) -> Item:
         table = self._db.table(collection)
-        query = Query()
-
-        item_dicts = table.search(query[Item.ITEM_ID_KEY] == item_id)
+        item_dicts = table.search(where(Item.ITEM_ID_KEY) == item_id)
         if len(item_dicts) < 1:
             raise ItemNotFoundError(f"Item ( {item_id} ) not found.")
 
