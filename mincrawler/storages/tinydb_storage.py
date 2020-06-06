82965d0ba42dba4de40ebfa07b7c6dc567ac66f4
altescy <altescy@fastmail.com>
2020-06-06 14:08:55 +0900

    modify tinydb operations

--- mincrawler/storages/tinydb_storage.py
+++ mincrawler/storages/tinydb_storage.py
@@ -1,3 +1,4 @@
+import typing as tp
 from tinydb import TinyDB, where
 
 from mincrawler.item import Item
@@ -7,6 +8,9 @@ from mincrawler.storages.storage import ItemDuplicationError, ItemNotFoundError
 
 @Storage.register("tinydb")
 class TinyDBStorage(Storage):
+    """
+    WARNING: TinyDBStorage is not thread-safe.
+    """
     def __init__(self, path: str) -> None:
         self._path = path
         self._db = TinyDB(path)
@@ -24,13 +28,13 @@ class TinyDBStorage(Storage):
 
     def exists(self, collection: str, item: Item) -> bool:
         table = self._db.table(collection)
-        items = table.search(where(Item.ITEM_ID_KEY) == item.id)
-        return len(items) > 0
+        exists = table.contains(where(Item.ITEM_ID_KEY) == item.id)
+        return tp.cast(bool, exists)
 
     def get(self, collection: str, item_id: str) -> Item:
         table = self._db.table(collection)
-        item_dicts = table.search(where(Item.ITEM_ID_KEY) == item_id)
-        if len(item_dicts) < 1:
+        item_dict = table.get(where(Item.ITEM_ID_KEY) == item_id)
+        if item_dict is None:
             raise ItemNotFoundError(f"Item ( {item_id} ) not found.")
 
-        return Item.from_dict(item_dicts[0])
+        return Item.from_dict(item_dict)
