ea51ccc1d62372a7858eab7994d3c30bfa08680e
altescy <altescy@fastmail.com>
2020-08-05 17:24:13 +0900

    make tinydb thread-safe

--- mincrawler/storages/tinydb_storage.py
+++ mincrawler/storages/tinydb_storage.py
@@ -1,30 +1,57 @@
+from __future__ import annotations
+import os
+from threading import Lock
 import typing as tp
+
 from tinydb import TinyDB, where
+from tinyrecord import transaction
 
 from mincrawler.item import Item
 from mincrawler.storages.storage import Storage
 from mincrawler.storages.storage import ItemDuplicationError, ItemNotFoundError
+from mincrawler.utils.locked_dict import LockedDict
 
 
 @Storage.register("tinydb")
 class TinyDBStorage(Storage):
-    """
-    WARNING: TinyDBStorage is not thread-safe.
-    """
+    _locks: LockedDict[str, tp.Tuple[Lock, TinyDB]] = LockedDict()
+
     def __init__(self, path: str) -> None:
-        self._path = path
-        self._db = TinyDB(path)
+        path = os.path.normpath(os.path.abspath(path))
+
+        with TinyDBStorage._locks as locks:
+            if path not in locks:
+                locks[path] = Lock(), TinyDB(path)
+            self._lock, self._db = locks[path]
+
+    def __enter__(self) -> TinyDBStorage:
+        self._lock.acquire()
+        return self
+
+    def __exit__(self, *args) -> None:
+        self._lock.release()
 
     def insert(self, collection: str, item: Item) -> None:
         if self.exists(collection, item):
             raise ItemDuplicationError(f"Item id {item.id} already exists.")
 
         table = self._db.table(collection)
-        table.insert(item.to_dict())
+        with transaction(table) as tx:
+            tx.insert(item.to_dict())
+
+    def update(self, collection: str, item: Item) -> None:
+        if not self.exists(collection, item):
+            raise ItemNotFoundError(f"Item ( {item.id} ) not found.")
+
+        table = self._db.table(collection)
+        with transaction(table) as tx:
+            tx.update(item.to_dict(), where(Item.ITEM_ID_KEY) == item.id)
 
     def upsert(self, collection: str, item: Item) -> None:
-        table = self._db.table(collection)
-        table.upsert(item.to_dict(), where(Item.ITEM_ID_KEY) == item.id)
+        if self.exists(collection, item):
+            self.update(collection, item)
+        else:
+            self.insert(collection, item)
 
     def exists(self, collection: str, item: Item) -> bool:
         table = self._db.table(collection)
--- tests/storages/test_tinydb_storage.py
+++ tests/storages/test_tinydb_storage.py
@@ -1,3 +1,4 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import tempfile
 
 from mincrawler.item import Item
@@ -19,3 +20,25 @@ class TestFileStorage:
 
             assert storage.get("test", "123")["text"] == "foo"
             assert storage.get("test", "456")["text"] == "bar"
+
+    @staticmethod
+    def test_multithread():
+        def task(path, taskid):
+            storage = TinyDBStorage(path)
+            with storage as s:
+                s.insert("test", Item(str(taskid), {"task": taskid}))
+            return taskid
+
+        with tempfile.TemporaryDirectory() as root:
+            path = root + "/test.json"
+            with ThreadPoolExecutor(max_workers=5) as executor:
+                futures = [
+                    executor.submit(task, path, taskid) for taskid in range(10)
+                ]
+                for future in as_completed(futures):
+                    taskid = future.result()
+                    print(f"Task {taskid} done.")
+
+            storage = TinyDBStorage(path)
+            item = storage.get("test", "1")
+            assert item["task"] == 1
